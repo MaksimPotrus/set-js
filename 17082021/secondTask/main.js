d27a1039e92c96c43b94291071e53303a755a6d7
potrus.m21 <potrus.m21@cf.dnu.dp.ua>
2021-08-18 12:49:46 +0300

    second commit3

--- 17082021/secondTask/main.js
+++ 17082021/secondTask/main.js
@@ -29,6 +29,40 @@ function converter(item, from, to) {
     if(from == 'LN' && to == 'M'){return item * 0.0021666666666};
     if(from == 'CL' && to == 'M'){return item * 0.000254};
     if(from == 'MIL' && to == 'M'){return item * 0.0000254};
+
+    if(from == 'LEA' && to == 'MI'){return item * 3};
+    if(from == 'MI' && to == 'FU'){return item * 8};
+    if(from == 'FUR' && to == 'MI'){return item * 1/8};
+    if(from == 'CH' && to == 'LI'){return item * 100};
+    if(from == 'RD' && to == 'LI'){return item * 25};
+    if(from == 'YD' && to == 'MEFU'){return item * 3};
+    if(from == 'CU' && to == 'YA'){return item * 0.5};
+    if(from == 'FT' && to == 'DU'){return item * 12};
+    if(from == 'SP' && to == 'PAL'){return item * 3};
+    if(from == 'HD' && to == 'DU'){return item * 4};
+    if(from == 'LN' && to == 'DU'){return item * 1/12};
+    if(from == 'CL' && to == 'DU'){return item * 0.01};
+    if(from == 'MIL' && to == 'DU'){return item * 0.001};
+    
+    if(from == 'LEA' && to == 'FU'){return item * 24};
+    if(from == 'MI' && to == 'YD'){return item * 1760};
+    if(from == 'FUR' && to == 'CH'){return item * 10};
+    if(from == 'CH' && to == 'FU'){return item * 1/10};
+    if(from == 'RD' && to == 'YD'){return item * 5.5};
+    if(from == 'YD' && to == 'DM'){return item * 36};
+    if(from == 'CU' && to == 'FU'){return item * 1.5};
+    if(from == 'SP' && to == 'DM'){return item * 9};
+
+    if(from == 'MI' && to == 'FU'){return item * 5280};
+    if(from == 'FUR' && to == 'YD'){return item * 220};
+    if(from == 'CH' && to == 'RO'){return item * 4};
+
+    if(from == 'FUR' && to == 'RO'){return item * 40};
+    if(from == 'CH' && to == 'FU'){return item * 66};
+
+    if(from == 'FUR' && to == 'FU'){return item * 660};
+
+    if(from == 'FUR' && to == 'LI'){return item * 1000};
     //International maritime units
     if(from == 'NL' && to == 'M'){return item * 5556.0};
     if(from == 'NMI' && to == 'M'){return item * 1852.0};
